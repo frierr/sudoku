89cb93eb47f565cd750fcc743587c1fc3a40bff3
V <kominko.vitalik@gmail.com>
2023-01-27 08:46:08 +0200

    update
    better generation and added end game highlights

--- sudoku.js
+++ sudoku.js
@@ -52,23 +52,104 @@ function createBox(value, type, posX, posY) {
 }
 
 function generateField() {
-    var field = [
-        [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        [4, 5, 6, 7, 8, 9, 1, 2, 3],
-        [7, 8, 9, 1, 2, 3, 4, 5, 6],
-        [2, 3, 4, 5, 6, 7, 8, 9, 1],
-        [5, 6, 7, 8, 9, 1, 2, 3, 4],
-        [8, 9, 1, 2, 3, 4, 5, 6, 7],
-        [3, 4, 5, 6, 7, 8, 9, 1, 2],
-        [6, 7, 8, 9, 1, 2, 3, 4, 5],
-        [9, 1, 2, 3, 4, 5, 6, 7, 8]
-    ];
-    //rows
-    field = shuffleField(field);
-    //columns
-    field = transpose(field);
+    var field = [[],[],[],[],[],[],[],[],[]];
+    var pattern = [1, 2, 3, 4, 5, 6, 7, 8];
+    var row = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    var num = 0;
+    var top = false;
+    field[0] = copyArray(field[0], row);
+    //
+    row = shiftArray(row);
+    num = pattern[Math.floor(Math.random() * 5 + 3)];
+    pattern = arrayRemove(pattern, num);
+    field[num] = copyArray(field[num], row);
+    top = num < 6;
+    //
+    row = shiftArray(row);
+    if (top) {
+        num = pattern[Math.floor(Math.random() * 2 + 3)];
+        pattern = arrayRemove(pattern, num);
+        field[num] = copyArray(field[num], row);
+    } else {
+        num = pattern[Math.floor(Math.random() * 2 + 3)];
+        pattern = arrayRemove(pattern, num);
+        field[num] = copyArray(field[num], row);
+    }
+    //
+    row = shiftArray(row);
+    num = pattern[Math.round(Math.random())];
+    pattern = arrayRemove(pattern, num);
+    field[num] = copyArray(field[num], row);
+    //
+    row = shiftArray(row);
+    if (top) {
+        num = pattern[Math.round(Math.random()) + 1];
+        pattern = arrayRemove(pattern, num);
+        field[num] = copyArray(field[num], row);
+    } else {
+        num = pattern[Math.floor(Math.random()) + 3];
+        pattern = arrayRemove(pattern, num);
+        field[num] = copyArray(field[num], row);
+    }
+    //
+    row = shiftArray(row);
+    if (top) {
+        num = pattern[Math.round(Math.random()) + 2];
+        pattern = arrayRemove(pattern, num);
+        field[num] = copyArray(field[num], row);
+    } else {
+        num = pattern[Math.floor(Math.random()) + 1];
+        pattern = arrayRemove(pattern, num);
+        field[num] = copyArray(field[num], row);
+    }
+    //
+    row = shiftArray(row);
+    num = pattern[0];
+    pattern = arrayRemove(pattern, num);
+    field[num] = copyArray(field[num], row);
+    //
+    row = shiftArray(row);
+    if (top) {
+        num = pattern[0];
+        pattern = arrayRemove(pattern, num);
+        field[num] = copyArray(field[num], row);
+    } else {
+        num = pattern[1];
+        pattern = arrayRemove(pattern, num);
+        field[num] = copyArray(field[num], row);
+    }
+    //
+    row = shiftArray(row);
+    num = pattern[0];
+    pattern = arrayRemove(pattern, num);
+    field[num] = copyArray(field[num], row);
+    //
     field = shuffleField(field);
     return transpose(field);
+}
+
+function copyArray(target, array) {
+    target = [];
+    for (var i = 0; i < array.length; i++) {
+        target[i] = array[i];
+    }
+    return target;
+}
+
+function shiftArray(array) {
+    var result = [];
+    for (var i = 1; i < array.length; i++) {
+        result[i - 1] = array[i];
+    }
+    result[result.length] = array[0];
+    return result;
+}
+
+function arrayRemove(array, item) {
+    if (array.indexOf(item) > -1) {
+        array.splice(array.indexOf(item), 1);
+    }
+    return array;
 }
 
 function shuffleField(field) {
@@ -77,7 +158,7 @@ function shuffleField(field) {
         [field[3],field[4],field[5]],
         [field[6],field[7],field[8]]
     ];
-    shuffle(temp);
+    temp = shuffle(temp);
     field[0] = temp[0][0];
     field[1] = temp[0][1];
     field[2] = temp[0][2];
@@ -88,17 +169,17 @@ function shuffleField(field) {
     field[7] = temp[2][1];
     field[8] = temp[2][2];
     temp = [field[0],field[1],field[2]];
-    shuffle(temp);
+    temp = shuffle(temp);
     field[0] = temp[0];
     field[1] = temp[1];
     field[2] = temp[2];
     temp = [field[3],field[4],field[5]];
-    shuffle(temp);
+    temp = shuffle(temp);
     field[3] = temp[0];
     field[4] = temp[1];
     field[5] = temp[2];
     temp = [field[6],field[7],field[8]];
-    shuffle(temp);
+    temp = shuffle(temp);
     field[6] = temp[0];
     field[7] = temp[1];
     field[8] = temp[2];
@@ -153,10 +234,16 @@ function gameStart() {
 
 function checkWinCondition() {
     var fail = false;
-    for (var i = 0; i < mask.length && !fail; i++) {
-        for (var j = 0; j < mask[i].length && !fail; j++) {
+    document.getElementById("field").textContent = "";
+    for (var i = 0; i < mask.length; i++) {
+        for (var j = 0; j < mask[i].length; j++) {
             if(mask[i][j]) {
                 fail = sudoku[i][j] != game[i][j];
+                var elem = createBox(game[i][j], !mask[i][j], i, j);
+                elem.style.backgroundColor = (sudoku[i][j] != game[i][j] ? "red" : "lime");
+                document.getElementById("field").appendChild(elem);
+            } else {
+                document.getElementById("field").appendChild(createBox(sudoku[i][j], true, i, j));
             }
         }
     }
